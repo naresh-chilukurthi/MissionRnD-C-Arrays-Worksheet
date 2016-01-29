e1531b2d32a49d3af05b38b3e316df3bea8cdc0f
naresh117 <nareshchilukurthi@gmail.com>
2016-01-29 18:47:15 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -13,14 +13,54 @@ ERROR CASES: Return NULL for invalid inputs.
 NOTES:
 */
 
-#include <iostream>
+#include <stdio.h>
 #include <malloc.h>
 
 struct student {
 	char *name;
 	int score;
 };
-
+void mystrcpy1(char *dst, const char *src)
+{
+while(*dst++=*src++);
+}
 struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+	int i,j;struct student temp;
+	if(students==NULL||len<=0)
+        return NULL;
+    else
+    {   struct student** ret=(student**)malloc(sizeof(student));
+        for(i=0;i<K;i++)
+        {
+            for(j=0;j<len-i-1;j++)
+            {
+                if(students[j].score<students[j+1].score)
+                {
+                    mystrcpy1(temp.name,students[j].name);
+                    //while(*(temp.name)++=*(students[j].name)++);
+                    temp.score =students[j].score;
+                    //student[j].name=(char*)malloc(sizeof(student[j+1].name)+1)
+                    mystrcpy1((students[j].name),(students[j+1].name));
+                    students[j].score= students[j+1].score;
+                    mystrcpy1(students[j+1].name ,temp.name);
+                    students[j+1].score = temp.score;
+                }
+            }
+            mystrcpy1(ret[i]->name,students[i].name);
+            ret[i]->score=students[i].score;
+        }
+        return ret;
+    }
+
+}
+int main()
+{
+    struct student students[3] = { { "stud1", 70 }, { "stud2", 60 }, { "stud3", 50 } };
+    int i;
+    struct student **ans;
+    ans=topKStudents(students, 3, 2);
+    for(i=0;i<2;i++)
+    {
+        printf("name%s marks %d\n",ans[i]->name,ans[i]->score);
+    }
 }
